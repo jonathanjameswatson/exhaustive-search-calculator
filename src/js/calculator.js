90440b6735b923f8e8557ea09559efa66ea1fce5
Jonathan Watson <jonathanw12@outlook.com>
2019-11-03 19:56:56 +0000

    Made website work again

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,3 +1,5 @@
+import generatorics from 'generatorics';
+
 const tuple1Box = document.getElementById('tuple-1');
 const tupleType = document.getElementById('tuple-type');
 const outputBox = document.getElementById('output-box');
@@ -16,7 +18,7 @@ const keywords = {
 const whiteSpace = /\s/g;
 const letterN = /n/;
 const nonCharacters = /\D/;
-const acceptable = /[^-+*/()d&|!=><.%(?:Xn,)]/g;
+const acceptable = /[^-+*/()d&|!=><.%(?:Xn,)0-9]/g;
 const keywordsRegex = new RegExp(Object.keys(keywords).join('|'), 'g');
 const xNs = /Xn,\d+/g;
 const digits = /\d+/;
@@ -46,7 +48,7 @@ const changeTextBox = (text) => {
 const check = () => {
   const tuple1 = tuple1Box.value.replace(whiteSpace, '').split(',');
   const k = Number(kBox.value.replace(letterN, tuple1.length).replace(nonCharacters, ''));
-  const xGenerator = G.clone[tupleType.value](tuple1, k);
+  const xGenerator = generatorics.clone[tupleType.value](tuple1, k);
   let x = [...xGenerator].slice();
 
   let y = [];
@@ -58,6 +60,7 @@ const check = () => {
 
     x.forEach((xN) => {
       const command2 = command1.replace(xNs, input => (xN[input.match(digits)[0] - 1]));
+
       if (!eval(command2)) {
         y.push(xN);
       }
@@ -72,12 +75,6 @@ const check = () => {
   changeTextBox((x.join('\n')));
 };
 
-const add = (element) => {
-  const clone = element.parentElement.cloneNode(true);
-  element.parentElement.parentElement.appendChild(clone);
-  Array.from(document.getElementsByClassName('minus')).forEach((toShow) => { toShow.hidden = false; });
-};
-
 const minus = (element) => {
   steps = document.getElementsByClassName('step');
   if (steps.length > 1) {
@@ -88,4 +85,22 @@ const minus = (element) => {
   }
 };
 
+const add = (element) => {
+  const clone = element.parentElement.cloneNode(true);
+  element.parentElement.parentElement.appendChild(clone);
+  Array.from(document.getElementsByClassName('minus')).forEach((button) => {
+    button.setAttribute('hidden', false);
+  });
+};
+
 changeTextBox('');
+
+document.addEventListener('click', (event) => {
+  if (event.target.matches('.add')) {
+    add(event.target);
+  } else if (event.target.matches('.minus')) {
+    minus(event.target);
+  } else if (event.target.matches('.check')) {
+    check();
+  }
+});
